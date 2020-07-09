c5120fd7a73822ffb791ed4fb0385752b8672cf3
camelCasingGod <57607665+camelCasingGod@users.noreply.github.com>
2020-07-09 13:13:19 -0700

    Filtering Products by Category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -8,15 +10,29 @@ import { CategoryService } from '../category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
-  constructor(productService: ProductService, categoryService: CategoryService) {
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService) {
 
-    this.products$ = productService.getAll();
+    productService.getAll().subscribe(products => this.products = products);
 
     this.categories$ = categoryService.getAll();
-    console.log(this.categories$);
+
+
+    route.queryParamMap.subscribe(params => {
+
+      this.category = params.get('category');
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category) :
+        this.products;
+
+    });
 
   }
 
