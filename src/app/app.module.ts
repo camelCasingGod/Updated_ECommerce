fbdd446387314890a2a9b20a79357112d215e473
camelCasingGod <57607665+camelCasingGod@users.noreply.github.com>
2020-07-14 11:22:22 -0700

    Redirecting the User to the Order Success Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -71,7 +71,7 @@ import { OrderService } from './order.service';
       { path: 'login', component: LoginComponent },
       
       { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
-      { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
+      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuard] },
       { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
       
 
--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -6,6 +6,7 @@ import { Subscription } from 'rxjs';
 import { OrderService } from '../order.service';
 import { AuthService } from '../auth.service';
 import { Order } from '../models/order';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check-out',
@@ -21,6 +22,7 @@ export class CheckOutComponent implements OnInit {
   userSubscription: Subscription;
 
   constructor(
+    private router: Router,
     private authService: AuthService,
     private shoppingCartService: ShoppingCartService,
     private orderService: OrderService) {}
@@ -29,7 +31,7 @@ export class CheckOutComponent implements OnInit {
 
     let cart = await this.shoppingCartService.getCart();
     this.cartSubscription = cart.subscribe(cart => this.cart = cart);
-    this.authService.user$.subscribe(user => this.userId = user.uid);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
 
   }
 
@@ -40,10 +42,11 @@ export class CheckOutComponent implements OnInit {
 
   }
 
-  placeOrder() {
+  async placeOrder() {
 
     let order = new Order(this.userId, this.shipping, this.cart);
-    this.orderService.storeOrder(order);
+    let result = await this.orderService.storeOrder(order);
+    this.router.navigate(['/order-success', result.key]);
 
   }
 
